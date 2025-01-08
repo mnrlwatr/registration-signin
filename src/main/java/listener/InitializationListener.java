0925d383443cad9a159ec7962e563a2b669a46a7
AdmSyd <adm_syd@icloud.com>
2025-01-08 06:36:25 +0500

    hibernate dependency injection

--- src/main/java/listener/InitializationListener.java
+++ src/main/java/listener/InitializationListener.java
@@ -5,6 +5,8 @@ import jakarta.servlet.ServletContext;
 import jakarta.servlet.ServletContextEvent;
 import jakarta.servlet.ServletContextListener;
 import jakarta.servlet.annotation.WebListener;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import service.UserService;
 
@@ -14,6 +16,8 @@ public class InitializationListener implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
         ServletContext servletContext = sce.getServletContext();
-        servletContext.setAttribute("userService", new UserService(new BCryptPasswordEncoder(),new UserDAO()));
+
+        SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+        servletContext.setAttribute("userService", new UserService(new BCryptPasswordEncoder(),new UserDAO(sessionFactory)));
     }
 }
