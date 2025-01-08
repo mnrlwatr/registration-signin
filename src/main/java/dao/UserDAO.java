e51b3bc1225a95163b6f4c74300da9e94de4c1bf
AdmSyd <adm_syd@icloud.com>
2025-01-08 06:34:52 +0500

    добавил методы для работы с бд

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -1,29 +1,53 @@
 package dao;
 
 import entity.User;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
 
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.Set;
-import java.util.stream.Stream;
+import java.util.Optional;
+import java.util.UUID;
 
+@RequiredArgsConstructor
 public class UserDAO {
 
-    private static final Set<User> usersSet = new HashSet<>();
+    private final SessionFactory sessionFactory;
 
-    public boolean addUser (User user) {
-        return usersSet.add(user);
+    public boolean insertUser(User user) {
+        Transaction transaction = null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction = session.beginTransaction();
+            session.persist(user);
+            transaction.commit();
+            return true;
+        } catch (HibernateException he) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            return false;
+        }
     }
 
-    public boolean deleteUser (User user){
-        return usersSet.remove(user);
+    public Optional<User> findUserByEmail(String email) {
+        User user =null;
+        try (Session session = sessionFactory.openSession()) {
+            user = session.get(User.class, email);
+        } catch (HibernateException he) {
+            he.printStackTrace();
+        }
+        return Optional.ofNullable(user);
+    }
+    public Optional<User> findUserById(UUID id) {
+        User user =null;
+        try (Session session = sessionFactory.openSession()) {
+            user=session.find(User.class,id);
+        } catch (HibernateException he) {
+            he.printStackTrace();
+        }
+        return Optional.ofNullable(user);
     }
 
-    public Stream<User> getUsersStream (){
-        return usersSet.stream();
-    }
 
-    public Iterator<User> getUsersIterator (){
-        return usersSet.iterator();
-    }
 }
