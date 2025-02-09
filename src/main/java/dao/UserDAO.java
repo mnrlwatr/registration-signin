fa88a7618ab6eb0fbc2463291e39c1109470610a
AdmSyd <adm_syd@icloud.com>
2025-02-09 05:38:04 +0500

    добавил crud методы для работы с бд

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -2,10 +2,7 @@ package dao;
 
 import entity.User;
 import lombok.RequiredArgsConstructor;
-import org.hibernate.HibernateException;
-import org.hibernate.Session;
-import org.hibernate.SessionFactory;
-import org.hibernate.Transaction;
+import org.hibernate.*;
 import org.hibernate.query.MutationQuery;
 import org.hibernate.query.Query;
 
@@ -39,7 +36,7 @@ public class UserDAO {
             query.setParameter("email", email);
             user=query.uniqueResult();
         } catch (HibernateException he) {
-            he.printStackTrace();
+            throw new RuntimeException(he);
         }
         return Optional.ofNullable(user);
     }
@@ -48,7 +45,7 @@ public class UserDAO {
         try (Session session = sessionFactory.openSession()) {
             user=session.find(User.class,id);
         } catch (HibernateException he) {
-            he.printStackTrace();
+            throw new RuntimeException(he);
         }
         return Optional.ofNullable(user);
     }
@@ -83,6 +80,29 @@ public class UserDAO {
                 transaction.rollback();
             }
             return false;
+        }
+    }
+
+    public boolean updateUser (User user) {
+        Transaction transaction=null;
+        try (Session session = sessionFactory.openSession()) {
+            transaction=session.beginTransaction();
+            session.merge(user);
+            transaction.commit();
+            return true;
+        } catch(HibernateException he) {
+            if(transaction != null) {
+                transaction.rollback();
+            }
+            return false;
+        }
+    }
+
+    public User getReference (UUID id){
+        try (Session session = sessionFactory.openSession()) {
+          return session.getReference(User.class,id);
+        } catch(HibernateException he) {
+            throw new RuntimeException(he);
         }
     }
 
