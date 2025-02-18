cf8611bc17af653fa96d299d6173c2b441ac6faf
AdmSyd <adm_syd@icloud.com>
2025-02-19 02:19:59 +0500

    added custom exception

---
+++ src/main/java/exceptions/invalidData/InvalidLastnameException.java
@@ -0,0 +1,8 @@
+package exceptions.invalidData;
+
+public class InvalidLastnameException extends InvalidUsersDataException {
+
+    protected InvalidLastnameException(String message) {
+        super(message);
+    }
+}
